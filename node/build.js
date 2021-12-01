61fee37522e9ad2e1a2e9d220149ef0e48614a2e
Tyler Smith <tylerlwsmith@gmail.com>
2021-12-01 00:59:06 -0800

    Dockerfile added. Testing build on Netlify.

---
+++ node/build.js
@@ -0,0 +1,61 @@
+const path = require("path");
+const chromium = require("chrome-aws-lambda");
+const puppeteer = require("puppeteer-core");
+const app = require("./app");
+
+(async function build() {
+  /** Set the script to crash on unhandled promise rejections. */
+  process.on("unhandledRejection", (error) => {
+    throw error;
+  });
+
+  /** Kill the process if it is still active after approximately thirty sec. */
+  const timeout = setTimeout(function () {
+    throw new Error("Process timed out after one minute.");
+  }, 30_000);
+
+  /** Wait for the server to start on a random available port. */
+  const server = await new Promise(function (resolve) {
+    const server = app.listen(0, function () {
+      console.log(`Listening on http://localhost:${server.address().port}`);
+      resolve(server);
+    });
+  });
+
+  /** Set up Puppeteer, navigate to the server and generate a screenshot. */
+  console.log("Launching Puppeteer");
+  browser =
+    process.env.NODE_ENV === "development"
+      ? await puppeteer.launch({
+          headless: true,
+          executablePath: "/usr/bin/chromium",
+          args: ["--no-sandbox"],
+        })
+      : await chromium.puppeteer.launch({
+          headless: true,
+          executablePath: await chromium.executablePath,
+          args: ["--no-sandbox"],
+        });
+
+  console.log("Generating screenshot");
+  const page = await browser.newPage();
+  await page.goto(`http://localhost:${server.address().port}/open-graph`);
+  await page.setViewport({ width: 1200, height: 630 });
+  await page.screenshot({
+    path: path.resolve(__dirname, "../public/generated/og-image.png"),
+    fullPage: true,
+  });
+  await browser.close();
+
+  /** Close the server. */
+  console.log("Shutting down server");
+  await new Promise(function (resolve) {
+    server.close(() => resolve());
+  });
+
+  /** Clear the timeout to prevent Node from hanging. */
+  clearTimeout(timeout);
+
+  /** Exit the app. */
+  process.exit(0);
+})();
