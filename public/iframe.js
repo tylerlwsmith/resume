b3b85a5bc17345fe217db8f2d4e316e35d48c949
Tyler Smith <tylerlwsmith@gmail.com>
2021-12-02 22:49:12 -0800

    Built iframe compatibility for external embedding

---
+++ public/iframe.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const urlParams = new URLSearchParams(window.location.search);
+  if (urlParams.get("iframe") === null) return;
+
+  document.addEventListener("load", messageHeight);
+  const resizeObserver = new ResizeObserver(messageHeight);
+  resizeObserver.observe(document.body, { box: "border-box" });
+
+  function messageHeight() {
+    const height = document.body.scrollHeight;
+    window.top.postMessage(height, "*");
+  }
+
+  const styleTag = document.createElement("style");
+  styleTag.innerHTML = `
+    /* Style overrides */
+    :root {
+      --color__primary: #59389b;
+    }
+
+    body {
+      background: transparent;
+      height: initial;
+    }
+  `;
+  document.head.appendChild(styleTag);
+});
--- public/transparent-background.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const urlParams = new URLSearchParams(window.location.search);
-  if (urlParams.get("transparent-background") === null) return;
-  document.body.style.backgroundColor = "transparent";
-});
