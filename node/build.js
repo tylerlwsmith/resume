e5a29a43a301a437c42d473f96f55fb9d2e5ae34
Tyler Smith <tylerlwsmith@gmail.com>
2021-12-02 17:42:12 -0800

    Awaiting asset generation + additional build logs

--- node/build.js
+++ node/build.js
@@ -3,14 +3,20 @@ const fse = require("fs-extra");
 const generateAssets = require("./generate-assets");
 const { homepage } = require("./templates");
 
-(function build() {
+(async function build() {
+  console.log("Clearing directory folder if it exists ...");
   fse.emptyDirSync(path.resolve(__dirname, "../build"));
+
+  console.log("Creating directory for generated assets ...");
   fse.emptyDirSync(path.resolve(__dirname, "../build/generated"));
   fse.copySync(
     path.resolve(__dirname, "../public"),
     path.resolve(__dirname, "../build")
   );
 
-  generateAssets();
+  console.log("Generating assets ...");
+  await generateAssets();
+
+  console.log("Copying rendered homepage into build directory ...");
   fse.writeFileSync(path.resolve(__dirname, "../build/index.html"), homepage());
 })();
--- node/server.js
+++ node/server.js
@@ -1,7 +1,7 @@
 const app = require("./app");
 const generateAssets = require("./generate-assets");
 
-const server = app.listen(process.env.PORT || 3000, function () {
+const server = app.listen(process.env.PORT || 3000, async function () {
   console.log(`Listening on http://localhost:${server.address().port}`);
-  generateAssets();
+  await generateAssets();
 });
