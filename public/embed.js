268e978c5e31e5623912367459a2b276ea21864b
Tyler Smith <tylerlwsmith@gmail.com>
2021-12-20 15:08:09 -0800

    Added a width attribute to the iframe wrapper

--- public/embed.js
+++ public/embed.js
@@ -7,6 +7,7 @@
   wrapper.classList.add("tyler-smith-resume-wrapper");
   wrapper.style.boxSizing = "content-box";
   wrapper.style.border = "1px solid transparent";
+  wrapper.style.width = "100%";
   wrapper.style.maxWidth = "8.5in";
   wrapper.style.minHeight = "11in";
   wrapper.style.background = "#fff";
